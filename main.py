64d80946f75a7884ef7d81ee943a8a3f4030164f
AchlanDev <achlandev@gmail.com>
2025-05-10 22:04:58 -0400

    Feat: Add asteroid.py, asteroidfield.py, player.py, update main.py, circleshape.py, constants.py
    - Added asteroid.py to create asteroids
    
    - Added asteroidfield.py to create drawable asteroid field
    
    - Added player.py to draw player to screen and move player on screen
    and use those groups to update the player and draw the screen
    
    - Updated main.py to use groups and containers
    
    - Updated circleshape.py to detect collisions
    
    - Updated constants.py to set Player speeds

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import pygame
 from constants import *
+from player import Player
+from circleshape import CircleShape
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 # Asteroids Game
 # This is a simple implementation of the classic Asteroids game using Pygame.
@@ -20,7 +24,22 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
-    
+    #Groups
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    # Containers
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+
+    # Set player
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    # Set asteroid field
+    asteroid_field = AsteroidField()
+
     # Game Loop
     # Game will run until the user quits
     # The game loop will handle events, update the game state, and render the screen
@@ -35,6 +54,13 @@ def main():
 
         # Fill the screen with black
         screen.fill((0, 0, 0))
+
+        # Draw the player
+        for sprite in drawable:
+            sprite.draw(screen)
+
+        # Update the player
+        updatable.update(dt)
 
         # Update the display
         pygame.display.flip()
